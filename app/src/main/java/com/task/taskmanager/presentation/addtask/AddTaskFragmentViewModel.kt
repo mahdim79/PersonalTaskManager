81c116ddc6e9330601bb9b941c0ef3b473a8b5b9
Mahdi <mahdimostafavi79@yahoo.com>
2025-04-16 20:30:15 +0330

    add new task fragment ui.add task to db

--- app/src/main/java/com/task/taskmanager/presentation/addtask/AddTaskFragmentViewModel.kt
+++ app/src/main/java/com/task/taskmanager/presentation/addtask/AddTaskFragmentViewModel.kt
@@ -1,9 +1,29 @@
 package com.task.taskmanager.presentation.addtask
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.task.core.domain.task.Task
+import com.task.core.helper.DataState
+import com.task.core.interactors.AddTask
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AddTaskFragmentViewModel @Inject constructor(): ViewModel() {
+class AddTaskFragmentViewModel @Inject constructor(private val addTask: AddTask) : ViewModel() {
+
+    val addTaskLiveData = MutableLiveData<DataState<Unit>>()
+
+    fun addNewTask(task: Task){
+        viewModelScope.launch(Dispatchers.IO) {
+            addTask.invoke(task)
+                .catch { addTaskLiveData.postValue(DataState.LocalError(it.message)) }
+                .collect{
+                    addTaskLiveData.postValue(it)
+                }
+        }
+    }
 }
--- app/src/main/java/com/task/taskmanager/presentation/mainactivity/MainActivity.kt
+++ app/src/main/java/com/task/taskmanager/presentation/mainactivity/MainActivity.kt
@@ -2,13 +2,20 @@ package com.task.taskmanager.presentation.mainactivity
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import com.task.core.helper.DataState
+import com.task.core.interactors.GetLocalTasks
 import com.task.taskmanager.R
 import com.task.taskmanager.databinding.ActivityMainBinding
 import com.task.taskmanager.presentation.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {

b165e5c4c8280aa546961017342f8cd3191c15c5
Mahdi <mahdimostafavi79@yahoo.com>
2025-04-16 14:32:37 +0330

    provide usecases

--- app/src/main/java/com/task/taskmanager/di/modules/UseCaseModule.kt
+++ app/src/main/java/com/task/taskmanager/di/modules/UseCaseModule.kt
@@ -1,4 +1,40 @@
 package com.task.taskmanager.di.modules
 
+import com.task.core.data.repositories.TaskRepository
+import com.task.core.interactors.AddTask
+import com.task.core.interactors.GetLocalTasks
+import com.task.core.interactors.GetRemoteTasks
+import com.task.core.interactors.RemoveTask
+import com.task.core.interactors.UpdateTask
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class UseCaseModule {
+    @Provides
+    @Singleton
+    fun provideAddTask(taskRepository: TaskRepository): AddTask = AddTask(taskRepository)
+
+    @Provides
+    @Singleton
+    fun provideRemoveTask(taskRepository: TaskRepository): RemoveTask = RemoveTask(taskRepository)
+
+    @Provides
+    @Singleton
+    fun provideGetLocalTasks(taskRepository: TaskRepository): GetLocalTasks =
+        GetLocalTasks(taskRepository)
+
+    @Provides
+    @Singleton
+    fun provideGetRemoteTasks(taskRepository: TaskRepository): GetRemoteTasks =
+        GetRemoteTasks(taskRepository)
+
+    @Provides
+    @Singleton
+    fun provideUpdateTask(taskRepository: TaskRepository): UpdateTask = UpdateTask(taskRepository)
+
 }

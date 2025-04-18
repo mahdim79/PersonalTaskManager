99fbb055ec6b5092aa306130a9bbd62b2e69a86a
Mahdi <mahdimostafavi79@yahoo.com>
2025-04-18 22:21:02 +0330

    add transitions

--- app/src/main/java/com/task/taskmanager/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/task/taskmanager/presentation/home/HomeFragment.kt
@@ -1,8 +1,10 @@
 package com.task.taskmanager.presentation.home
 
 import android.app.AlertDialog
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,16 +16,18 @@ import com.task.core.domain.task.Task
 import com.task.core.helper.DataState
 import com.task.taskmanager.R
 import com.task.taskmanager.TaskApplication
+import com.task.taskmanager.databinding.DialogTaskDetailsBinding
 import com.task.taskmanager.databinding.FragmentHomeBinding
-import com.task.taskmanager.presentation.addtask.adapters.MainTaskRecyclerViewAdapter
+import com.task.taskmanager.presentation.home.adapter.MainTaskRecyclerViewAdapter
 import com.task.taskmanager.presentation.base.BaseFragment
 import com.task.taskmanager.presentation.utils.TaskAction
 import com.task.taskmanager.utils.AlarmHandler
 import com.task.taskmanager.utils.ArgumentKeys
-import com.task.taskmanager.utils.SettingManager
+import com.task.taskmanager.utils.Utils
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.runBlocking
+import java.util.Calendar
 import javax.inject.Inject
+import androidx.core.graphics.drawable.toDrawable
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment() {
@@ -109,21 +113,53 @@ class HomeFragment : BaseFragment() {
         mBinding.rvHomeTasks.layoutManager =
             LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
 
-        taskRecyclerViewAdapter.setOnActionClickListener { task, action ->
-            when (action) {
-                TaskAction.ACTION_EDIT -> {
-                    task.id?.let { tId ->
-                        findNavController().navigate(
-                            R.id.action_homeFragment_to_updateTaskFragment,
-                            Bundle().apply { putInt(ArgumentKeys.KEY_TASK_ID, tId) })
+        taskRecyclerViewAdapter.apply {
+            setOnActionClickListener { task, action ->
+                when (action) {
+                    TaskAction.ACTION_EDIT -> {
+                        task.id?.let { tId ->
+                            findNavController().navigate(
+                                R.id.action_homeFragment_to_updateTaskFragment,
+                                Bundle().apply { putInt(ArgumentKeys.KEY_TASK_ID, tId) })
+                        }
                     }
-                }
 
-                TaskAction.ACTION_REMOVE -> showRemoveTaskDialog(task)
+                    TaskAction.ACTION_REMOVE -> showRemoveTaskDialog(task)
+                }
+            }
+
+            setOnItemClickListener {
+                showTaskDetailsDialog(it)
             }
         }
 
         mBinding.rvHomeTasks.adapter = taskRecyclerViewAdapter
+    }
+
+    private fun showTaskDetailsDialog(task: Task) {
+        val dialog = Dialog(requireContext())
+        val view = DialogTaskDetailsBinding.inflate(layoutInflater)
+        view.apply {
+            btnTaskDetailsOk.setOnClickListener {
+                dialog.dismiss()
+            }
+            tvTaskDetailsTitle.text = task.title
+            tvTaskDetailsDescription.text = task.description
+            tvTaskDetailsTime.text = Utils.convertCalendarTimeToShowFormat(Calendar.getInstance().apply { timeInMillis = task.time })
+        }
+
+        dialog.apply {
+            setContentView(view.root)
+
+            val width = (resources.displayMetrics.widthPixels * 0.9).toInt()
+            val dialogParams: ViewGroup.LayoutParams = view.cvTaskDetailsContainer.layoutParams
+            dialogParams.width = width
+            view.cvTaskDetailsContainer.layoutParams = dialogParams
+
+            window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+            window?.setDimAmount(0.4f)
+            dialog.show()
+        }
     }
 
     private fun showRemoveTaskDialog(task: Task) {

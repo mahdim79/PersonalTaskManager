cc245cd59dff9f73170ecb5efe79aa021a5d5969
Mahdi <mahdimostafavi79@yahoo.com>
2025-04-19 14:31:27 +0330

    add worker initial delay

--- app/src/main/java/com/task/taskmanager/utils/Constants.kt
+++ app/src/main/java/com/task/taskmanager/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.task.taskmanager.utils
 object Constants {
     // api
     const val BASE_URL = "https://run.mocky.io/v3/"
-    const val ENDPOINT_GET_ALL_TASKS = "7ac897ec-bce6-4315-835a-344bb23ff10b"
+    const val ENDPOINT_GET_ALL_TASKS = "e168946a-1dce-4e3d-823b-bc47f0c640a0"
 
     // local db
     const val DB_NAME = "task_local_db"

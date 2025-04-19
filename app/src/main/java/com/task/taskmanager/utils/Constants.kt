0a1838fdfacc876d517d2861a234bbb108e5a617
Mahdi <mahdimostafavi79@yahoo.com>
2025-04-19 23:38:50 +0330

    some change in mock api

--- app/src/main/java/com/task/taskmanager/utils/Constants.kt
+++ app/src/main/java/com/task/taskmanager/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.task.taskmanager.utils
 object Constants {
     // api
     const val BASE_URL = "https://run.mocky.io/v3/"
-    const val ENDPOINT_GET_ALL_TASKS = "e168946a-1dce-4e3d-823b-bc47f0c640a0"
+    const val ENDPOINT_GET_ALL_TASKS = "ae67e727-860a-4197-95ea-0d19d816c1b7"
     const val APP_CENTER_DSN = "d4eaa161-53dd-44b2-9322-957e436c6f67"
 
     // local db
